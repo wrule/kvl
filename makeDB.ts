8365369c2d53f4e9341e9ab006299935ab923943
jimao <shihao.gu@perfma.com>
2024-09-18 14:48:10 +0800

    优化make和update

--- makeDB.ts
+++ makeDB.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import base64Text from './1.db';
 
 export default
-function makeDB(file: fs.PathOrFileDescriptor) {
+function makeDB(file: string) {
   fs.writeFileSync(file, Buffer.from(base64Text, 'base64'));
 }
 
